6b6fca8f70378b14be2c7ffe1a7ebd4881bffd9d
notsaad <saadmazharr@gmail.com>
2025-05-20 23:56:31 -0700

    functionalize routes
    Signed-off-by: notsaad <saadmazharr@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,51 @@
-import { Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Header from './components/Header';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Education from './components/Education';
-import Experience from './components/Experience';
-import { ThemeProvider } from './components/ThemeProvider';
-import ThemeToggle from './components/ThemeToggle';
-import Background from './components/Background';
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Header from "./components/Header";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Education from "./components/Education";
+import Experience from "./components/Experience";
+import { ThemeProvider } from "./components/ThemeProvider";
+import ThemeToggle from "./components/ThemeToggle";
+import Background from "./components/Background";
+import {SaadGPT} from "./components/saadgpt";
+
 
 const projects = [
   {
     title: "Research Paper: HABO Framework for Hyperparameter Optimization",
-    description: "A research paper in the field of reinforcment learning. Supervised by Dr. Tom Cesari at the University of Ottawa, EECS department.",
-    link: "https://arxiv.org/abs/2503.10282"
+    description:
+      "A research paper in the field of reinforcment learning. Supervised by Dr. Tom Cesari at the University of Ottawa, EECS department.",
+    link: "https://arxiv.org/abs/2503.10282",
   },
   {
     title: "Custom Interpreter",
-    description: "A lightweight interpreter from scratch for a custom programming language, supporting basic arithmetic operations, control flow, and data types.",
-    link: "https://github.com/notsaad/interpreter"
+    description:
+      "A lightweight interpreter from scratch for a custom programming language, supporting basic arithmetic operations, control flow, and data types.",
+    link: "https://github.com/notsaad/interpreter",
   },
   {
     title: "Sustain App",
-    description: "React Native mobile app for the uOttaHack 6 Hackathon calculating clothing items’ environmental impact via quantitative analysis based on a clothing tag’s information",
-    link: "https://github.com/notsaad/uOttaHack2024"
+    description:
+      "React Native mobile app for the uOttaHack 6 Hackathon calculating clothing items’ environmental impact via quantitative analysis based on a clothing tag’s information",
+    link: "https://github.com/notsaad/uOttaHack2024",
   },
 ];
 
 const contactLinks = [
   {
     href: "mailto:saadmazhar@me.com",
-    text: "Email Me"
+    text: "Email Me",
   },
   {
     href: "https://www.github.com/notsaad",
     text: "GitHub",
-    external: true
+    external: true,
   },
   {
     href: "https://www.linkedin.com/in/notsaad",
     text: "LinkedIn",
-    external: true
+    external: true,
   },
 ];
 
@@ -48,8 +53,8 @@ const education = [
   {
     institution: "University of Ottawa",
     degree: "Honors BS in Computer Science, Minor in Philosophy",
-    period: "September 2021 - December 2025"
-  }
+    period: "September 2021 - April 2026",
+  },
 ];
 
 const experiences = [
@@ -59,8 +64,9 @@ const experiences = [
     location: "Palo Alto, California",
     period: "January 2025 - August 2025",
     description: [
-      'Worked full-stack on Tesla’s electrical distribution management system servicing hundreds of engineers worldwide using Go, React TypeScript, GraphQL and Kubernetes.',
-      'Led development of a new schematic tool using Slate.js to streamline the production of electrical drawings for 10+ engineering teams.',
+      "Worked full-stack on Tesla’s electrical distribution management system servicing 10,000+ engineers worldwide using Go, React TypeScript, GraphQL and Kubernetes.",
+      "Leveraged goroutines and channels to implement concurrent data processing pipelines, significantly improving the throughput and responsiveness of backend services.",
+      "Wrote efficient GraphQL queries to return data from a MySQL database with millions of records.",
     ],
   },
   {
@@ -69,9 +75,9 @@ const experiences = [
     location: "Ottawa, Ontario",
     period: "May 2024 - August 2024",
     description: [
-      'Developed Python automation scripts to eliminate redundant In-Service Software Upgrade (ISSU) functions and structures in a large legacy C codebase, reducing manual effort by approximately 92%.',
-      'Streamlined codebase maintenance by automating the identification and removal of obsolete ISSU components, processing over 100,000 lines of code.',
-      'Optimized test suite performance by removing redundant ISSU functions and structures, reducing overall test execution time by 3% and enabling more frequent integration testing cycles and CI/CD Pipelines.'
+      "Developed Python automation scripts to eliminate redundant In-Service Software Upgrade (ISSU) functions and structures in a large legacy C codebase, reducing manual effort by approximately 92%.",
+      "Streamlined codebase maintenance by automating the identification and removal of obsolete ISSU components, processing over 100,000 lines of code.",
+      "Optimized test suite performance by removing redundant ISSU functions and structures, reducing overall test execution time by 3% and enabling more frequent integration testing cycles and CI/CD Pipelines.",
     ],
   },
   {
@@ -80,9 +86,9 @@ const experiences = [
     location: "Ottawa, Ontario",
     period: "September 2023 - December 2023",
     description: [
-      'Built and maintained an internal order tracking system using Vue.js, MySQL, Node.js, Express.',
-      'Implemented efficient API calls using Axios to interact with a MySQL database hosted on MariaDB, optimizing query performance and reducing backend response time by 30%',
-      'Authored Bash scripts that automatically configured accurate IP/TCP protocols for newly updated beds.'
+      "Built and maintained an internal order tracking system using Vue.js, MySQL, Node.js, Express.",
+      "Implemented efficient API calls using Axios to interact with a MySQL database hosted on MariaDB, optimizing query performance and reducing backend response time by 30%",
+      "Authored Bash scripts that automatically configured accurate IP/TCP protocols for newly updated beds.",
     ],
   },
   {
@@ -91,15 +97,14 @@ const experiences = [
     location: "Ottawa, Ontario",
     period: "May 2023 - August 2023",
     description: [
-      'Designed and implemented a robust C++ enumeration system to efficiently translate hex codes into corresponding error messages, reducing error interpretation time by 90%.',
-      'Optimized SQL database schema for parsed log data, resulting in a 50% improvement in query response times.',
-      'Created a RESTful API using Node.js to serve parsed error data to a Vue.js frontend, enabling real-time error monitoring and analysis.'
+      "Designed and implemented a robust C++ enumeration system to efficiently translate hex codes into corresponding error messages, reducing error interpretation time by 90%.",
+      "Optimized SQL database schema for parsed log data, resulting in a 50% improvement in query response times.",
+      "Created a RESTful API using Node.js to serve parsed error data to a Vue.js frontend, enabling real-time error monitoring and analysis.",
     ],
-  }
-
+  },
 ];
 
-function HomePage() {
+const HomePage = () => {
   return (
     <Layout>
       <ThemeToggle />
@@ -114,21 +119,9 @@ function HomePage() {
       <Contact links={contactLinks} />
     </Layout>
   );
-}
+};
 
-function SaadGPTPage() {
-  return (
-    <Layout>
-      <ThemeToggle />
-      <div>
-        <h1>SAADGPT</h1>
-        <p>Coming soon...</p>
-      </div>
-    </Layout>
-  );
-}
-
-function ResumePage() {
+const ResumePage = () => {
   return (
     <Layout>
       <ThemeToggle />
@@ -138,19 +131,19 @@ function ResumePage() {
       </div>
     </Layout>
   );
-}
+};
 
-function App() {
+const App = () => {
   return (
     <ThemeProvider>
       <Background />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/saadgpt" element={<SaadGPTPage />} />
+        <Route path="/saadgpt" element={<SaadGPT />} />
         <Route path="/resume" element={<ResumePage />} />
       </Routes>
     </ThemeProvider>
   );
-}
+};
 
 export default App;
---
+++ src/components/saadgpt/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const SaadGPT = () => {
+    return (
+        <div>
+            <h1>Welcome to SaadGPT</h1>
+            <p>This is a functional component.</p>
+        </div>
+    );
+};
